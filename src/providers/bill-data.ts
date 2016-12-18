e7918a3194e779bf9e0309c3afe662b5a1326a9a
Jorge Vergara <javebratt@gmail.com>
2016-12-18 14:30:56 -0500

    Updates the BillData provider to RC4

--- src/providers/bill-data.ts
+++ src/providers/bill-data.ts
@@ -1,18 +1,39 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
-import 'rxjs/add/operator/map';
+import {
+  AngularFire,
+  FirebaseListObservable,
+  FirebaseObjectObservable } from 'angularfire2';
 
-/*
-  Generated class for the BillData provider.
-
-  See https://angular.io/docs/ts/latest/guide/dependency-injection.html
-  for more info on providers and Angular 2 DI.
-*/
 @Injectable()
 export class BillData {
+  billList: FirebaseListObservable<any>;
+  billDetail: FirebaseObjectObservable<any>;
+  userId: string;
 
-  constructor(public http: Http) {
-    console.log('Hello BillData Provider');
+  constructor(public af: AngularFire) {
+    af.auth.subscribe( auth => {
+      if (auth){
+        this.billList = af.database.list(`/userProfile/${auth.uid}/billList`);
+        this.userId = auth.uid;
+      }
+    });
   }
+
+  getBillList(): FirebaseListObservable<any> { return this.billList; }
+
+  getBill(billId: string): FirebaseObjectObservable<any> {
+    return this.billDetail = this.af.database
+      .object(`/userProfile/${this.userId}/billList/${billId}`);
+  }
+
+  createBill(name: string, amount: number, dueDate: string = null, paid: boolean = false){
+    return this.billList.push({ name, amount, dueDate });
+  }
+
+  removeBill(billId: string): any { return this.billList.remove(billId); }
+
+  payBill(billId: string): any { return this.billList.update(billId, {paid: true}); }
+
+  
 
 }
