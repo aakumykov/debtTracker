c58b9b33b4e2b85005bf393561d1d163f7da7d8a
Jorge Vergara <javebratt@gmail.com>
2016-12-18 15:07:41 -0500

    Updates the BillCreate class to RC4

--- src/pages/create-bill/create-bill.ts
+++ src/pages/create-bill/create-bill.ts
@@ -1,22 +1,47 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController } from 'ionic-angular';
+import { FormBuilder, Validators } from '@angular/forms';
+import { BillData } from '../../providers/bill-data';
 
-/*
-  Generated class for the CreateBill page.
-
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
 @Component({
   selector: 'page-create-bill',
-  templateUrl: 'create-bill.html'
+  templateUrl: 'create-bill.html',
 })
 export class CreateBillPage {
+  public newBillForm;
+  nameChanged: boolean = false;
+  amountChanged: boolean = false;
+  submitAttempt: boolean = false;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  constructor(public navCtrl: NavController, public billData: BillData, 
+    public formBuilder: FormBuilder) {
+    
+    this.newBillForm = formBuilder.group({
+      name: ['', Validators.required],
+      amount: ['', Validators.required],
+    });
+  }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad CreateBillPage');
+  elementChanged(input){
+    let field = input.inputControl.name;
+    this[field + "Changed"] = true;
+  }
+
+  createBill(){
+    this.submitAttempt = true;
+
+    if (!this.newBillForm.valid){
+      console.log(this.newBillForm.value);
+    } else {
+      this.billData.createBill(this.newBillForm.value.name, 
+        this.newBillForm.value.amount,this.newBillForm.value.dueDate)
+        .then( () => {
+        this.navCtrl.pop();
+      }, error => {
+        console.log(error);
+      });
+
+    }
   }
 
 }
